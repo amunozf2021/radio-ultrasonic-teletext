57d204f7e6d253e1bb40d8cfffe2a19738d2387f
amunozf2021 <91094909+amunozf2021@users.noreply.github.com>
2025-12-30 00:21:53 +0100

    test ipHone

--- emitter/generate.js
+++ emitter/generate.js
@@ -2,9 +2,9 @@ import fs from "fs";
 import * as WavEncoder from "wav-encoder";
 
 const sampleRate = 44100;
-const bitDuration = 0.07;
-const f0 = 17000;
-const f1 = 17500;
+const bitDuration = 0.1;
+const f0 = 16800;
+const f1 = 17600;
 const volume = 0.3;
 
 // ---------- CRC-8 ----------
